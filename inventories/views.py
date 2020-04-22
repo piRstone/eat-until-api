77fd1de4aaca612cc1c3e1c94e1c684ad1db9b47
Pierre Lavalley <pierre.lavalley@gmail.com>
2020-04-22 19:43:47 +0200

    Get list products sorted by expiration_date

--- inventories/views.py
+++ inventories/views.py
@@ -24,6 +24,17 @@ class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
     permission_classes = (permissions.IsAuthenticated, ProductPermission)
 
+    def get_queryset(self):
+        items = []
+
+        # Filter agains inventory_id ID
+        inventory_id = self.request.query_params.get('inventory_id', None)
+
+        if inventory_id is not None:
+            items = Product.objects.filter(inventory_id=inventory_id).order_by('expiration_date')
+
+        return items
+
     def perform_create(self, serializer):
         """Set the user as owner"""
         serializer.save(user=self.request.user)
